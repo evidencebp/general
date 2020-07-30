e0022de012f9abaa26252b8ca888f5f83f70f22b
idanacumen <idan@acumen.io>
2020-07-30 10:01:10 +0300

    Updated repo profile

--- queries/repo_properties.sql
+++ queries/repo_properties.sql
@@ -5,40 +5,34 @@ create table
 general.repo_properties_per_year
 as
 select
-cf.repo_name as repo_name
-, extract( year from cf.commit_month) as year
-, min(cf.commit_timestamp) as min_commit_time
-, min(cf.commit) as min_commit
-, max(extension) as extension
-, max(code_extension) as code_extension
-, max(is_test) as is_test
-, count(distinct cf.commit) as commits
-, count(distinct if(parents = 1, cf.commit, null)) as non_merge_commits
-, count(distinct case when cf.is_corrective  then cf.commit else null end) as corrective_commits
-, if(count(distinct if(parents = 1, cf.commit, null)) > 0
-    ,1.0*count(distinct case when cf.is_corrective and parents = 1 then cf.commit else null end)/
-        count(distinct if(parents = 1, cf.commit, null))
+repo_name as repo_name
+, extract( year from commit_month) as year
+, min(commit_timestamp) as min_commit_time
+, max(commit_timestamp) as max_commit_time
+, min(commit) as min_commit
+, count(distinct commit) as commits
+, count(distinct if(parents = 1, commit, null)) as non_merge_commits
+, count(distinct case when is_corrective  then commit else null end) as corrective_commits
+, if(count(distinct if(parents = 1, commit, null)) > 0
+    ,1.0*count(distinct case when is_corrective and parents = 1 then commit else null end)/
+        count(distinct if(parents = 1, commit, null))
     , null) as corrective_rate
-, if(count(distinct if(parents = 1, cf.commit, null)) > 0
-    , 1.253*count(distinct case when cf.is_corrective  and parents = 1 then cf.commit else null end)/
-        count(distinct if(parents = 1, cf.commit, null)) -0.053
+, if(count(distinct if(parents = 1, commit, null)) > 0
+    , 1.253*count(distinct case when is_corrective  and parents = 1 then commit else null end)/
+        count(distinct if(parents = 1, commit, null)) -0.053
     , null ) as ccp
-, avg(if(not cf.is_corrective and parents = 1, non_test_files, null)) as avg_coupling_size
-, avg(if(not cf.is_corrective and parents = 1, code_non_test_files, null)) as avg_coupling_code_size
-, avg(if(not cf.is_corrective and parents = 1, if(non_test_files > 103 , 103 , non_test_files), null)) as avg_coupling_size_capped
-, avg(if(not cf.is_corrective and parents = 1, if(code_non_test_files> 103 , 103 ,code_non_test_files), null)) as avg_coupling_code_size_capped
-, avg(if(not cf.is_corrective and parents = 1, if(non_test_files > 103 , null , non_test_files), null)) as avg_coupling_size_cut
-, avg(if(not cf.is_corrective and parents = 1, if(code_non_test_files> 103 , null ,code_non_test_files), null)) as avg_coupling_code_size_cut
-, count(distinct cf.Author_email) as authors
-, max(cf.Author_email) as Author_email # Meaningful only when authors=1
+, avg(if(not is_corrective and parents = 1, non_test_files, null)) as avg_coupling_size
+, avg(if(not is_corrective and parents = 1, code_non_test_files, null)) as avg_coupling_code_size
+, avg(if(not is_corrective and parents = 1, if(non_test_files > 103 , 103 , non_test_files), null)) as avg_coupling_size_capped
+, avg(if(not is_corrective and parents = 1, if(code_non_test_files> 103 , 103 ,code_non_test_files), null)) as avg_coupling_code_size_capped
+, avg(if(not is_corrective and parents = 1, if(non_test_files > 103 , null , non_test_files), null)) as avg_coupling_size_cut
+, avg(if(not is_corrective and parents = 1, if(code_non_test_files> 103 , null ,code_non_test_files), null)) as avg_coupling_code_size_cut
+, count(distinct Author_email) as authors
+, max(Author_email) as Author_email # Meaningful only when authors=1
+, avg(if(same_date_as_prev, duration, null)) as same_date_duration
 , min(ec.commit_month) as commit_month
 from
-general.commits_files as cf
-join
 general.enhanced_commits as ec
-on
-cf.commit = ec.commit and cf.repo_name = ec.repo_name
-and extract( year from cf.commit_month) =  extract( year from ec.commit_month)
 group by
 repo_name
 , year
@@ -51,38 +45,33 @@ create table
 general.repo_properties
 as
 select
-cf.repo_name as repo_name
-, min(cf.commit_timestamp) as min_commit_time
-, min(cf.commit) as min_commit
-, max(extension) as extension
-, max(code_extension) as code_extension
-, max(is_test) as is_test
-, count(distinct cf.commit) as commits
-, count(distinct if(parents = 1, cf.commit, null)) as non_merge_commits
-, count(distinct case when cf.is_corrective  then cf.commit else null end) as corrective_commits
-, if(count(distinct if(parents = 1, cf.commit, null)) > 0
-    ,1.0*count(distinct case when cf.is_corrective and parents = 1 then cf.commit else null end)/
-        count(distinct if(parents = 1, cf.commit, null))
+repo_name as repo_name
+, min(commit_timestamp) as min_commit_time
+, max(commit_timestamp) as max_commit_time
+, min(commit) as min_commit
+, count(distinct commit) as commits
+, count(distinct if(parents = 1, commit, null)) as non_merge_commits
+, count(distinct case when is_corrective  then commit else null end) as corrective_commits
+, if(count(distinct if(parents = 1, commit, null)) > 0
+    ,1.0*count(distinct case when is_corrective and parents = 1 then commit else null end)/
+        count(distinct if(parents = 1, commit, null))
     , null) as corrective_rate
-, if(count(distinct if(parents = 1, cf.commit, null)) > 0
-    , 1.253*count(distinct case when cf.is_corrective  and parents = 1 then cf.commit else null end)/
-        count(distinct if(parents = 1, cf.commit, null)) -0.053
+, if(count(distinct if(parents = 1, commit, null)) > 0
+    , 1.253*count(distinct case when is_corrective  and parents = 1 then commit else null end)/
+        count(distinct if(parents = 1, commit, null)) -0.053
     , null ) as ccp
-, avg(if(not cf.is_corrective and parents = 1, non_test_files, null)) as avg_coupling_size
-, avg(if(not cf.is_corrective and parents = 1, code_non_test_files, null)) as avg_coupling_code_size
-, avg(if(not cf.is_corrective and parents = 1, if(non_test_files > 103 , 103 , non_test_files), null)) as avg_coupling_size_capped
-, avg(if(not cf.is_corrective and parents = 1, if(code_non_test_files> 103 , 103 ,code_non_test_files), null)) as avg_coupling_code_size_capped
-, avg(if(not cf.is_corrective and parents = 1, if(non_test_files > 103 , null , non_test_files), null)) as avg_coupling_size_cut
-, avg(if(not cf.is_corrective and parents = 1, if(code_non_test_files> 103 , null ,code_non_test_files), null)) as avg_coupling_code_size_cut
-, count(distinct cf.Author_email) as authors
-, max(cf.Author_email) as Author_email # Meaningful only when authors=1
+, avg(if(not is_corrective and parents = 1, non_test_files, null)) as avg_coupling_size
+, avg(if(not is_corrective and parents = 1, code_non_test_files, null)) as avg_coupling_code_size
+, avg(if(not is_corrective and parents = 1, if(non_test_files > 103 , 103 , non_test_files), null)) as avg_coupling_size_capped
+, avg(if(not is_corrective and parents = 1, if(code_non_test_files> 103 , 103 ,code_non_test_files), null)) as avg_coupling_code_size_capped
+, avg(if(not is_corrective and parents = 1, if(non_test_files > 103 , null , non_test_files), null)) as avg_coupling_size_cut
+, avg(if(not is_corrective and parents = 1, if(code_non_test_files> 103 , null ,code_non_test_files), null)) as avg_coupling_code_size_cut
+, count(distinct Author_email) as authors
+, max(Author_email) as Author_email # Meaningful only when authors=1
+, avg(if(same_date_as_prev, duration, null)) as same_date_duration
 , min(ec.commit_month) as commit_month
 from
-general.commits_files as cf
-join
 general.enhanced_commits as ec
-on
-cf.commit = ec.commit and cf.repo_name = ec.repo_name
 group by
 repo_name
 ;
