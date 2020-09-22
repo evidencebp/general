2fa36d1c9cb2f8f98c883b6a6375753eb7726705
idanacumen <idan@acumen.io>
2020-09-22 16:12:26 +0300

    More general_ght tables

--- ght/queries/fetch_public_data.sql
+++ ght/queries/fetch_public_data.sql
@@ -81,3 +81,115 @@ general_ght.pull_requests as pr
 on
 prh.pull_request_id = pr.id
 ;
+
+
+drop table if exists general_ght.pull_request_comments;
+
+create table
+general_ght.pull_request_comments
+AS
+select
+prc.*
+from
+`ghtorrent-bq.ght.pull_request_comments` as prc
+join
+general_ght.pull_requests as pr
+on
+prc.pull_request_id = pr.id
+;
+
+
+drop table if exists general_ght.commit_comments;
+
+create table
+general_ght.commit_comments
+AS
+select
+cc.*
+from
+`ghtorrent-bq.ght.commit_comments` as cc
+join
+general_ght.commits as c
+on
+cc.commit_id = c.id
+;
+
+
+drop table if exists general_ght.issues;
+
+create table
+general_ght.issues
+AS
+select
+i.*
+from
+general_ght.projects as p
+join
+`ghtorrent-bq.ght.issues` as i
+on
+p.id = i.repo_id
+;
+
+
+drop table if exists general_ght.project_languages;
+
+create table
+general_ght.project_languages
+AS
+select
+pl.*
+from
+general_ght.projects as p
+join
+`ghtorrent-bq.ght.project_languages` as pl
+on
+p.id = pl.project_id
+;
+
+
+drop table if exists general_ght.project_members;
+
+create table
+general_ght.project_members
+AS
+select
+pm.*
+from
+general_ght.projects as p
+join
+`ghtorrent-bq.ght.project_members` as pm
+on
+p.id = pm.repo_id
+;
+
+
+drop table if exists general_ght.users;
+
+create table
+general_ght.users
+AS
+select
+u.*
+from
+general_ght.project_members as pm
+join
+`ghtorrent-bq.ght.users` as u
+on
+pm.user_id = u.id
+;
+
+
+drop table if exists general_ght.issue_events;
+
+create table
+general_ght.issue_events
+AS
+select
+ie.*
+from
+general_ght.issues as i
+join
+`ghtorrent-bq.ght.issue_events` as ie
+on
+i.id = ie.issue_id
+;
