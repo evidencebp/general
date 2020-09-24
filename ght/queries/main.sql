9a2673c6bcd5a2a6802f63ef6a95470a16eb2daf
idanacumen <idan@acumen.io>
2020-09-24 17:11:38 +0300

    Added references to descriptions

--- ght/queries/main.sql
+++ ght/queries/main.sql
@@ -1,3 +1,10 @@
+# See data set description here
+# https://www.gousios.gr/pub/ghtorrent-dataset-toolsuite.pdf
+# https://ghtorrent.org/
+
+# Data set at BigQuery
+# https://bigquery.cloud.google.com/dataset/ghtorrent-bq:ght
+
 # Main.sql
 # Creation instruction
 # 1. Use repo data from general
