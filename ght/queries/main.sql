2fcd89c74894a590e993b8b760ed8b9378034338
idanacumen <idan@acumen.io>
2020-10-22 11:31:48 +0300

    Updated flow in main.sql

--- ght/queries/main.sql
+++ ght/queries/main.sql
@@ -10,3 +10,8 @@
 # 1. Use repo data from general
 # 2. Use linguistic procedures from general
 # 3. Run fetch_public_data.sql
+# 4. Run enhanced_issues.sql
+# 5. Run enhanced_pull_requests.sql
+# 6. Run merged_enhanced_pull_requests.sql
+# 7. Run effective_pull_requests.sql
+# 8. Run repo_properties.sql
