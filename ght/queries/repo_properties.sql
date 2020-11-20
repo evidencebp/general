f62637ebaa9a4bcadf72a05f0be1ba9d7c80f907
idanacumen <idan@acumen.io>
2020-11-20 10:33:53 +0200

    Fixed GHT per year properties so they consider the year. A good idea

--- ght/queries/repo_properties.sql
+++ ght/queries/repo_properties.sql
@@ -61,11 +61,13 @@ p.repo_name
 
 drop table if exists general_ght.repo_profile;
 
+drop table if exists general_ght.repo_profile;
+
 create table
 general_ght.repo_profile
 as
 select
-p.repo_name as repo_name
+rp.*
 , ip.issues
 , ip.assigned_issues
 , ip.assigned_issues_ratio
@@ -93,8 +95,23 @@ p.repo_name as repo_name
 , prp.open_to_merge_minutes
 , prp.first_commit_to_merge_minutes
 , prp.sloppy_pr_ratio
+
+# Productivity metrics
+, ip.merged_issues/rp.authors as merged_issues_per_developer
+, if(rp.involved_developers > 0
+        , ip.merged_issues/rp.involved_developers
+        , null)as merged_issues_per_involved_developer
+, prp.merged_prs/rp.authors as merged_prs_per_developer
+, if(rp.involved_developers > 0
+        , prp.merged_prs/rp.involved_developers
+        , null)as merged_prs_per_involved_developer
+
 from
+general.repo_properties as rp
+join
 general_ght.projects as p
+on
+rp.repo_name = p.repo_name
 left join
 general_ght.repo_issues_profile as ip
 on
@@ -118,33 +135,56 @@ general_ght.repo_issues_profile_per_year
 as
 select
 p.repo_name as repo_name
-, extract(year from ie.created_at) as year
-, count(distinct issue_id) as issues
-, count(distinct if(ie.assigned_at is null, null, issue_id)) as assigned_issues
-, 1.0*count(distinct if(ie.assigned_at is null, null, issue_id))/count(distinct issue_id) as assigned_issues_ratio
-, count(distinct if(ie.closed_at is null, null, issue_id)) as closed_issues
-, 1.0*count(distinct if(ie.closed_at is null, null, issue_id))/count(distinct issue_id) as closed_issues_ratio
-, count(distinct if(ie.merged_at is null, null, issue_id)) as merged_issues
-, 1.0*count(distinct if(ie.merged_at is null, null, issue_id))/count(distinct issue_id) as merged_issues_ratio
-, count(distinct if(ie.reopened_at is null, null, issue_id)) as reopened_issues
-, 1.0*count(distinct if(ie.reopened_at is null, null, issue_id))/count(distinct issue_id) as reopened_issues_ratio
-, count(distinct if(ie. milestoned_at is null, null, issue_id)) as milestoned_issues
-, 1.0*count(distinct if(ie.milestoned_at is null, null, issue_id))/count(distinct issue_id) as milestoned_issues_ratio
-, count(distinct if(ie.demilestoned_at is null, null, issue_id)) as demilestoned_issues
-, 1.0*count(distinct if(ie.demilestoned_at is null, null, issue_id))/count(distinct issue_id) as demilestoned_issues_ratio
+, rpy.year as year
+, count(distinct if(extract(year from ie.created_at) = rpy.year, issue_id, null)) as issues
+, count(distinct if(extract(year from ie.assigned_at)= rpy.year , issue_id, null)) as assigned_issues
+, if(count(distinct if(extract(year from ie.created_at) = rpy.year, issue_id, null)) > 0
+, 1.0*count(distinct if(extract(year from ie.assigned_at)= rpy.year , issue_id, null))
+    /count(distinct if(extract(year from ie.created_at) = rpy.year, issue_id, null))
+, null ) as assigned_issues_ratio
+, count(distinct if(extract(year from ie.closed_at) = rpy.year, issue_id, null)) as closed_issues
+, if(count(distinct if(extract(year from ie.created_at) = rpy.year, issue_id, null)) > 0
+, 1.0*count(distinct if(extract(year from ie.closed_at) = rpy.year, issue_id, null))
+/count(distinct if(extract(year from ie.created_at) = rpy.year, issue_id, null))
+, null ) as closed_issues_ratio
+, count(distinct if(extract(year from ie.merged_at) = rpy.year, issue_id, null)) as merged_issues
+, if(count(distinct if(extract(year from ie.created_at) = rpy.year, issue_id, null)) > 0
+, 1.0*count(distinct if(extract(year from ie.merged_at) = rpy.year, issue_id, null))
+/count(distinct if(extract(year from ie.created_at) = rpy.year, issue_id, null))
+, null ) as merged_issues_ratio
+, count(distinct if(extract(year from ie.reopened_at) = rpy.year, issue_id, null)) as reopened_issues
+, if(count(distinct if(extract(year from ie.created_at) = rpy.year, issue_id, null)) > 0
+, 1.0*count(distinct if(extract(year from ie.reopened_at) = rpy.year, issue_id, null))
+/count(distinct if(extract(year from ie.created_at) = rpy.year, issue_id, null))
+, null ) as reopened_issues_ratio
+, count(distinct if(extract(year from ie.milestoned_at) = rpy.year, issue_id, null)) as milestoned_issues
+, if(count(distinct if(extract(year from ie.created_at) = rpy.year, issue_id, null)) > 0
+, 1.0*count(distinct if(extract(year from ie.milestoned_at) = rpy.year, issue_id, null))
+/count(distinct if(extract(year from ie.created_at) = rpy.year, issue_id, null))
+, null ) as milestoned_issues_ratio
+, count(distinct if(extract(year from ie.demilestoned_at) = rpy.year, issue_id, null)) as demilestoned_issues
+, if(count(distinct if(extract(year from ie.created_at) = rpy.year, issue_id, null)) > 0
+, 1.0*count(distinct if(extract(year from ie.demilestoned_at) = rpy.year, issue_id, null))
+/count(distinct if(extract(year from ie.created_at) = rpy.year, issue_id, null))
+, null ) as demilestoned_issues_ratio
 , avg( assigned_to_closed_minutes ) as assigned_to_closed_minutes
 , avg( created_to_assigned_minutes ) as created_to_assigned_minutes
 , avg( created_to_closed_minutes ) as created_to_closed_minutes
 from
-general_ght.enhanced_issues as ie
+general.repo_properties_per_year as rpy
 join
 general_ght.projects as p
+on
+rpy.repo_name = p.repo_name
+join
+general_ght.enhanced_issues as ie
 on
 ie.repo_id = p.id
 group by
 p.repo_name
-, extract(year from ie.created_at)
+, rpy.year
 ;
+
 
 
 drop table if exists general_ght.pull_requests_profile_per_year;
@@ -154,28 +194,45 @@ general_ght.pull_requests_profile_per_year
 as
 select
 p.repo_name as repo_name
-, extract(year from epr.opened_at) as year
-, count(distinct epr.id) as pull_requests
-, count(distinct if(epr.opened_at is null, null, epr.id)) as opened_prs
-, 1.0*count(distinct if(epr.opened_at is null, null, epr.id))/count(distinct epr.id) as opened_pr_ratio
-, count(distinct if(epr.closed_at is null, null, epr.id)) as closed_prs
-, 1.0*count(distinct if(epr.closed_at is null, null, epr.id))/count(distinct epr.id) as closed_pr_ratio
-, count(distinct if(epr.merged_at is null, null, epr.id)) as merged_prs
-, 1.0*count(distinct if(epr.merged_at is null, null, epr.id))/count(distinct epr.id) as merged_pr_ratio
-, avg(first_to_last_commit_minutes) as first_to_last_commit_minutes
-, avg(open_to_merge_minutes) as open_to_merge_minutes
-, avg(first_commit_to_merge_minutes) as first_commit_to_merge_minutes
-, 1.0*sum(if(days_to_first_bug <=7, 1,0))/sum(1) as sloppy_pr_ratio
+, rpy.year as year
+, count(distinct if(extract(year from epr.created_at) = rpy.year, epr.id, null)) as pull_requests
+, count(distinct if(extract(year from epr.opened_at) = rpy.year, epr.id, null)) as opened_prs
+, if(count(distinct if(extract(year from epr.created_at) = rpy.year, epr.id, null)) > 0
+, 1.0*count(distinct if(extract(year from epr.opened_at) = rpy.year, epr.id, null))/
+    count(distinct if(extract(year from epr.created_at) = rpy.year, epr.id, null))
+, null) as opened_pr_ratio
+, count(distinct if(extract(year from epr.closed_at) = rpy.year, epr.id, null)) as closed_prs
+, if(count(distinct if(extract(year from epr.created_at) = rpy.year, epr.id, null)) > 0
+, 1.0*count(distinct if(extract(year from epr.closed_at) = rpy.year, epr.id, null))/
+    count(distinct if(extract(year from epr.created_at) = rpy.year, epr.id, null))
+, null) as closed_pr_ratio
+, count(distinct if(extract(year from epr.merged_at) = rpy.year, epr.id, null)) as merged_prs
+, if(count(distinct if(extract(year from epr.created_at) = rpy.year, epr.id, null)) > 0
+, 1.0*count(distinct if(extract(year from epr.merged_at) = rpy.year, epr.id, null))/
+    count(distinct if(extract(year from epr.created_at) = rpy.year, epr.id, null))
+, null) as merged_pr_ratio
+, avg(if(extract(year from epr.created_at) = rpy.year, first_to_last_commit_minutes, null)) as first_to_last_commit_minutes
+, avg(if(extract(year from epr.created_at) = rpy.year,open_to_merge_minutes, null)) as open_to_merge_minutes
+, avg(if(extract(year from epr.created_at) = rpy.year,first_commit_to_merge_minutes, null)) as first_commit_to_merge_minutes
+, if(sum(if(extract(year from epr.created_at) = rpy.year,1, 0)) > 0
+, 1.0*sum(if(extract(year from epr.created_at) = rpy.year
+ and days_to_first_bug <=7, 1,0))/sum(if(extract(year from epr.created_at) = rpy.year,1, 0))
+, null) as sloppy_pr_ratio
 from
+general.repo_properties_per_year as rpy
+join
 general_ght.projects as p
+on
+rpy.repo_name = p.repo_name
 join
 general_ght.enhanced_pull_requests as epr
 on
 p.id = epr.base_repo_id
 group by
 p.repo_name
-, extract(year from epr.opened_at)
+, rpy.year
 ;
+
 
 drop table if exists general_ght.repo_properties_per_year;
 
@@ -212,6 +269,17 @@ rpy.*
 , prp.open_to_merge_minutes
 , prp.first_commit_to_merge_minutes
 , prp.sloppy_pr_ratio
+
+# Productivity metrics
+, ip.merged_issues/rpy.authors as merged_issues_per_developer
+, if(rpy.involved_developers > 0
+        , ip.merged_issues/rpy.involved_developers
+        , null)as merged_issues_per_involved_developer
+, prp.merged_prs/rpy.authors as merged_prs_per_developer
+, if(rpy.involved_developers > 0
+        , prp.merged_prs/rpy.involved_developers
+        , null)as merged_prs_per_involved_developer
+
 from
 general.repo_properties_per_year as rpy
 left join
