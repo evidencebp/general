cced1c6c39bd185faad31e42308f871318e06764
idanacumen <idan@acumen.io>
2021-01-04 09:55:54 +0200

    Repos per year - Standard SQL

--- queries/extract_repos_by_year.sql
+++ queries/extract_repos_by_year.sql
@@ -1,3 +1,5 @@
+# Legacy
+# Into 2020_above_50
 SELECT
   repo_name,
   COUNT(DISTINCT commit) AS commits,
@@ -16,5 +18,33 @@ HAVING
   COUNT(DISTINCT
     CASE
       WHEN YEAR(USEC_TO_TIMESTAMP(committer.date.seconds*1000000)) = 2020 THEN commit
-      ELSE NULL END) > 99
+      ELSE NULL END) >= 49
+;
+
+# Standard Sql
+drop table if exists general.2020_above_50;
+
+create table
+general.2020_above_50
+as
+SELECT
+  commit_repo_name as repo_name,
+  COUNT(DISTINCT commit) AS commits,
+  COUNT(DISTINCT
+    CASE
+      WHEN extract(year from TIMESTAMP_SECONDS(committer.date.seconds)) = 2020 THEN commit
+      ELSE NULL END) AS commits_2020,
+  COUNT(DISTINCT committer.email) AS commiters,
+  MIN(TIMESTAMP_SECONDS(committer.date.seconds)) AS start_time,
+  MAX(TIMESTAMP_SECONDS(committer.date.seconds)) AS end_time
+FROM
+  `bigquery-public-data.github_repos.commits`
+  cross join  UNNEST(repo_name) as commit_repo_name
+GROUP BY
+  commit_repo_name
+HAVING
+  COUNT(DISTINCT
+    CASE
+      WHEN extract(year from TIMESTAMP_SECONDS(committer.date.seconds)) = 2020 THEN commit
+      ELSE NULL END) >= 49
 ;
