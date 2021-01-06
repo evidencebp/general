052434f32a2224d2fcbe69a7df6ebe2083952b91
idanacumen <idan@acumen.io>
2021-01-06 09:27:28 +0200

    Removed legacy query from extract projects

--- queries/extract_repos_by_year.sql
+++ queries/extract_repos_by_year.sql
@@ -1,26 +1,3 @@
-# Legacy
-# Into 2020_above_50
-SELECT
-  repo_name,
-  COUNT(DISTINCT commit) AS commits,
-  COUNT(DISTINCT
-    CASE
-      WHEN YEAR(USEC_TO_TIMESTAMP(committer.date.seconds*1000000)) = 2020 THEN commit
-      ELSE NULL END) AS commits_2020,
-  COUNT(DISTINCT committer.email) AS commiters,
-  MIN(USEC_TO_TIMESTAMP(committer.date.seconds*1000000)) AS start_time,
-  MAX(USEC_TO_TIMESTAMP(committer.date.seconds*1000000)) AS end_time
-FROM
-  [bigquery-public-data:github_repos.commits]
-GROUP BY
-  Repo_name
-HAVING
-  COUNT(DISTINCT
-    CASE
-      WHEN YEAR(USEC_TO_TIMESTAMP(committer.date.seconds*1000000)) = 2020 THEN commit
-      ELSE NULL END) >= 49
-;
-
 # Standard Sql
 drop table if exists general.2020_above_50;
 
