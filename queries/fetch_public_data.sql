899a34f73ba4ef0a17d5bc254c0ed05c482ef03e
idanacumen <idan@acumen.io>
2021-01-12 10:04:07 +0200

    Optimized fetch public data

--- queries/fetch_public_data.sql
+++ queries/fetch_public_data.sql
@@ -42,15 +42,32 @@ drop table if exists general.commits;
 
 create table
 general.commits
+partition by
+commit_month
+cluster by
+repo_name, commit
 as
 select
-c.*
+r.repo_name as repo_name
+, commit
+, max(author.name) as author_name
+, max(author.email) as author_email
+, max(cast(FORMAT_DATE('%Y-%m-01', DATE(TIMESTAMP_SECONDS(author.date.seconds))) as date)) as  commit_month
+, max(TIMESTAMP_SECONDS(author.date.seconds)) as commit_timestamp
+, max(subject) as subject
+, max(message) as message
+, count(distinct parent) as parents
 from
 `bigquery-public-data.github_repos.commits` as c
 cross join  UNNEST(repo_name) as commit_repo_name
 Join
 general.repos as r
-On commit_repo_name = r.Repo_name
+On
+commit_repo_name = r.Repo_name
+cross join  UNNEST(parent) as parent
+group by
+r.repo_name
+, commit
 ;
 
 drop table if exists general.enhanced_commits;
@@ -63,39 +80,22 @@ cluster by
 repo_name, commit
 as
 select
-r.repo_name as repo_name
-, commit
-# Note - all the properties bellow make computations more efficient but do not exist in Google's scheme
-, max(author.name) as author_name
-, max(author.email) as author_email
-, max(cast(FORMAT_DATE('%Y-%m-01', DATE(TIMESTAMP_SECONDS(author.date.seconds))) as date)) as  commit_month
-, max(TIMESTAMP_SECONDS(author.date.seconds)) as commit_timestamp
-, max(subject) as subject
-, max(message) as message
-, count(distinct parent) as parents
-, max(general.bq_corrective(message) > 0) as is_corrective
-, max(general.bq_adaptive(message) > 0) as is_adaptive
-, max(general.bq_perfective(message) > 0) as is_perfective
-, max(general.bq_English(message) > 0) as is_English
-, max(general.bq_refactor(message) > 0) as is_refactor
-, max(general.bq_core_cursing(message) > 0) as is_cursing
+c.*
+, general.bq_corrective(message) > 0 as is_corrective
+, general.bq_adaptive(message) > 0 as is_adaptive
+, general.bq_perfective(message) > 0 as is_perfective
+, general.bq_English(message) > 0 as is_English
+, general.bq_refactor(message) > 0 as is_refactor
+, general.bq_core_cursing(message) > 0 as is_cursing
+# The values of the columns below will be overridden later
 , -1 as files
 , -1 as non_test_files
 , -1 as code_files
 , -1 as code_non_test_files
 , -1 as duration
 , commit as prev_commit
-, max(TIMESTAMP_SECONDS(author.date.seconds)) as prev_timestamp
+, commit_timestamp as prev_timestamp
 , False as same_date_as_prev
-
 from
-general.commits
-cross join  UNNEST(repo_name) as commit_repo_name
-Join
-general.repos as r
-On commit_repo_name = r.Repo_name
-cross join  UNNEST(parent) as parent
-group by
-r.repo_name
-, commit
+general.commits as c
 ;
