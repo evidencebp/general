07f1c6985a80dff76804c62a90f4e26a27e22aed
idanacumen <idan@acumen.io>
2021-03-04 15:16:17 +0200

    Added reverted commits to main

--- queries/main.sql
+++ queries/main.sql
@@ -1,16 +1,17 @@
 # Main.sql
 # Creation instruction
-# 1. Get repo data (e.g., from CCP)
-# 2. Create linguistic procedures - run commit_classification.sql
-# 3. Run fetch_public_data.sql
-# 4. Run commits_with_files.sql
-# 5. Run commit_size.sql
-# 6. Run commits_with_duration.sql
-# 7. Run component_familiarity.sql
-# 8. Run testing_pairs.sql
-# 9. Run file_properties.sql
-# 10. Run repo_properties.sql
-# 11. Run developer_properties.sql
-# 12. Run repo_productivity_metrics.sql
-# 13. Run churn_and_onboarding.sql
-# 14. Run commit_file_prev_touch.sql
+#  Get repo data (e.g., from CCP)
+#  Create linguistic procedures - run commit_classification.sql
+#  Run fetch_public_data.sql
+#  Run commits_with_files.sql
+#  Run commit_size.sql
+#  Run commits_with_duration.sql
+#  Run component_familiarity.sql
+#  Run testing_pairs.sql
+#  Run reverted_commits.sql
+#  Run file_properties.sql
+#  Run repo_properties.sql
+#  Run developer_properties.sql
+#  Run repo_productivity_metrics.sql
+#  Run churn_and_onboarding.sql
+#  Run commit_file_prev_touch.sql
